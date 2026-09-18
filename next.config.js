cc4e17bb9f5d54ec40840baf60dff103d642f242
vsvivek2006 <143882154+vsvivek2006@users.noreply.github.com>
2026-09-18 15:31:57 +0530

    fix: clean next.config.js image loader syntax

--- next.config.js
+++ next.config.js
@@ -25,12 +25,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      },
-    ],
-  },
   webpack: (config, { dev }) => {
     if (!dev) {
       config.cache = false;
